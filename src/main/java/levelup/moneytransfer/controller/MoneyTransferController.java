fa8021d6940c0180039c7d80417ea7a789e1ba9f
emokeeva <ekaterina.mokeeva@t-systems.com>
2020-10-19 23:36:15 +0300

    Initial commit

---
+++ src/main/java/levelup/moneytransfer/controller/MoneyTransferController.java
@@ -0,0 +1,9 @@
+package levelup.moneytransfer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //Это удобная аннотация, которая объединяет @ Controller и @ ResponseBody -
+// устраняет необходимость аннотировать каждый метод обработки запросов класса контроллера аннотацией @ ResponseBody
+
+public class MoneyTransferController {
+}
